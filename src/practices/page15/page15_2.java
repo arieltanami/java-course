caa151e6fdb51cffd86d3cce636aca7409c5c0c1
Ariel Tan <arieltan@edu.hac.ac.il>
2023-11-21 18:01:47 +0200

    Add page15 exercises (practices)

---
+++ src/practices/page15/page15_2.java
@@ -0,0 +1,10 @@
+package practices.page15;
+
+public class page15_2 {
+    // (2) כתוב תכנית שמדפיסה את סדרת המספרים: 1, 2, . . . , 25
+    public static void main(String[] args) {
+        for (int i = 1; i <= 25; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ src/practices/page15/page15_6.java
@@ -0,0 +1,13 @@
+package practices.page15;
+
+public class page15_6 {
+    // (6) כתוב תכנית שעוברת על כל המספרים בין 1-100
+    // ומדפיסה כל מספר שמתחלק ב-11 ללא שארית
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 11 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
